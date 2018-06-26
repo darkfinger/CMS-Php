a27347192a5749ead2f6949035117a6c6e93dc4e
darkfinger <892810dkcbak@G>
2018-06-26 15:16:28 -0400

    categories set in the sidebar of index

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -25,34 +25,37 @@
     </div>
 
     <!-- Blog Categories Well -->
+
+    <?php
+    $query = "select * from categories";
+    $categoriesForSidebar = mysqli_query($connection, $query);
+    ?>
     <div class="well">
         <h4>Blog Categories</h4>
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                    <?php
+                    while ($row = mysqli_fetch_assoc($categoriesForSidebar)) { ?>
+                        <li><a href="#"><?php echo $row['cat_title'] ?></a>
+                        </li>
+                    <?php } ?>
+
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
+<!--            <div class="col-lg-6">-->
+<!--                <ul class="list-unstyled">-->
+<!--                    <li><a href="#">Category Name</a>-->
+<!--                    </li>-->
+<!--                    <li><a href="#">Category Name</a>-->
+<!--                    </li>-->
+<!--                    <li><a href="#">Category Name</a>-->
+<!--                    </li>-->
+<!--                    <li><a href="#">Category Name</a>-->
+<!--                    </li>-->
+<!--                </ul>-->
+<!--            </div>-->
             <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
@@ -61,7 +64,8 @@
     <!-- Side Widget Well -->
     <div class="well">
         <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus
+            laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
     </div>
 
 </div>
--- index.php
+++ index.php
@@ -8,7 +8,6 @@ include "includes/navbar.php";
 ?>
 <!-- Page Content -->
 <div class="container">
-
     <div class="row">
 
         <!-- Blog Entries Column -->
@@ -22,47 +21,48 @@ include "includes/navbar.php";
                 if (!$searchQuerry) {
                     die("Querry failed" . mysqli_error($connection));
                 }
-                $countRow= mysqli_num_rows($searchQuerry);
-                if ($countRow<1){
+                $countRow = mysqli_num_rows($searchQuerry);
+                if ($countRow < 1) {
                     echo "<H1>No Result Found</H1>";
                 }
 
             } else {
                 $query = "select * from posts";
-                $searchQuerry = mysqli_query($connection, $query);}
+                $searchQuerry = mysqli_query($connection, $query);
+            }
 
-                while ($row = mysqli_fetch_assoc($searchQuerry)) {
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];
+            while ($row = mysqli_fetch_assoc($searchQuerry)) {
+                $post_title = $row['post_title'];
+                $post_author = $row['post_author'];
+                $post_date = $row['post_date'];
+                $post_image = $row['post_image'];
+                $post_content = $row['post_content'];
 
-                    ?>
+                ?>
 
 
-                    <h1 class="page-header">
-                        Page Heading
-                        <small>Secondary Text</small>
-                    </h1>
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
 
-                    <!-- First Blog Post -->
-                    <h2>
-                        <a href="#"><?php echo $post_title ?></a>
-                    </h2>
-                    <p class="lead">
-                        by <a href="index.php"><?php echo $post_author ?></a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
-                    <hr>
-                    <img class="img-responsive" src="images/post/<?php echo $post_image ?>"
-                         alt="<?php echo $post_title ?>">
-                    <hr>
-                    <p><?php echo $post_content ?></p>
-                    <a class="btn btn-primary" href="#">Read More <span
-                                class="glyphicon glyphicon-chevron-right"></span></a>
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="images/post/<?php echo $post_image ?>"
+                     alt="<?php echo $post_title ?>">
+                <hr>
+                <p><?php echo $post_content ?></p>
+                <a class="btn btn-primary" href="#">Read More <span
+                            class="glyphicon glyphicon-chevron-right"></span></a>
 
-                    <hr>
+                <hr>
 
                 <?php
             } ?>
